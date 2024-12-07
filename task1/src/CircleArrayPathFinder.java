3e3a72c62638a8c406fd7b17d0be037c8af3ea74
frontendLearning-31082022 <MorglHer@yandex.ru>
2024-12-07 21:05:56 +0300

    fix rename cause wrong info

--- task1/src/CircleArrayPathFinder.java
+++ task1/src/CircleArrayPathFinder.java
@@ -1,11 +1,8 @@
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-/**
- * 'task1' lower case cause its require at task description
- */
 
-class task1 {
+public class CircleArrayPathFinder {
     private int size;
 
     private int distance;
@@ -13,14 +10,14 @@ class task1 {
     int curStartPoint = 0;
     private ArrayList<Integer> orderFirstEls;
 
-    public task1(int size, int distance) {
+    public CircleArrayPathFinder(int size, int distance) {
         this.size = size;
         this.distance = distance - 1;
         orderFirstEls = new ArrayList();
     }
 
     public static void main(String[] args) {
-        System.out.println(new task1(Integer.valueOf(args[0]),
+        System.out.println(new CircleArrayPathFinder(Integer.valueOf(args[0]),
                 Integer.valueOf(args[1])).calculatePath());
     }
 
--- task1/test/CircleArrayPathFinderTest.java
+++ task1/test/CircleArrayPathFinderTest.java
@@ -1,30 +1,28 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class task1Test {
+class CircleArrayPathFinderTest {
 
     @org.junit.jupiter.api.Test
     void calculatePath_simple1() {
-        task1 circleArray=new task1(4,3);
+        CircleArrayPathFinder circleArray=new CircleArrayPathFinder(4,3);
         assert  circleArray.calculatePath().equals("13");
     }
     @org.junit.jupiter.api.Test
     void calculatePath_simple2() {
-        task1 circleArray=new task1(5,4);
+        CircleArrayPathFinder circleArray=new CircleArrayPathFinder(5,4);
         assert  circleArray.calculatePath().equals("14253");
     }
 
 
     @Test
     void getEndPoint_overStep() {
-        task1 circleArray=new task1(5,6);;
+        CircleArrayPathFinder circleArray=new CircleArrayPathFinder(5,6);;
         assert circleArray.getEndPoint()==0;
     }
 
     @Test
     void getEndPoint_overStep_2() {
-        task1 circleArray=new task1(5,9);;
+        CircleArrayPathFinder circleArray=new CircleArrayPathFinder(5,9);;
         assert circleArray.getEndPoint()==3;
     }
 }
